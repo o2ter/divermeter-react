7c3cbc16abb89752a22c5d629c6b1f2f3cae19c6
Susan Cheng <susan.doggie@gmail.com>
2023-08-19 12:58:46 +0800

    test

---
+++ server.config.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+
+module.exports = (env, argv) => ({
+  output: path.join(__dirname, 'test/dist'),
+  client: {
+    main: {
+      entry: './test/client/index.js',
+      uri: '/',
+    },
+  },
+  serverEntry: './test/server/index.js'
+})
